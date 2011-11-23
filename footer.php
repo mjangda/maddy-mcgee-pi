abb11dd4c7ff48b6c08e44e9b25ee9879069ce57
Peter N. Trinh <dayuse@gmail.com>
2011-11-23 00:03:53 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 		<div id="mm-footer">
-			<p class="mm-legal-footer"><a href="<?php echo home_url(); ?>">Maddy McGee, P.I.</a> by <a href="http://peterntrinh.tumblr.com/">Peter N. Trinh</a> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-nd/2.5/ca/">Creative Commons Attribution-NonCommercial-NoDerivs 2.5 Canada License</a>.</p>
+			<p class="mm-legal-footer"><a href="<?php echo home_url(); ?>">Maddy McGee, P.I.</a> by <a href="http://peterntrinh.tumblr.com/">Peter N. Trinh</a> & <a href="http://britt_ish.tumblr.com/">Britt Baker</a> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-nd/2.5/ca/">Creative Commons Attribution-NonCommercial-NoDerivs 2.5 Canada License</a>.
+			<br>Story, characters and art by Peter N. Trinh. Additional story by Britt Baker.</p>
 		</div>
 	</div>
 	<?php wp_footer(); ?>
