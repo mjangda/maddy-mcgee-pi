f48816e18478043dfebd5e2dc0f2478010d53494
Mohammad Jangda <batmoo@gmail.com>
2011-11-24 20:55:06 -0500

    Enabling Open Graph tags

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once( dirname( __FILE__ ) . '/includes/ComicManager.php' );
+include( dirname( __FILE__ ) . '/includes/wpcom-open-graph/wpcom-open-graph.php' );
 
 class MaddyMcGee {
 
