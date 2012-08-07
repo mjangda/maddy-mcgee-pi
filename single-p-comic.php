613d0ae28133d35f46377a8a0942473fb59b9458
Mohammad Jangda <batmoo@gmail.com>
2012-08-06 22:11:49 -0300

    Add comments support to comics

--- single-p-comic.php
+++ single-p-comic.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
 <?php get_template_part( 'loop', 'comic-feature' ); ?>
+
+<div id="mm-content">
+  <div id="mm-entries">
+		<div class="mm-comments">
+      <?php comments_template( '', true ); ?>
+	  </div>
+	</div>
+	<?php get_sidebar(); ?>
+</div>
+
 <?php get_footer(); ?>
