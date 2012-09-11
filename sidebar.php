b5f6374f71b4a493d1db20b965ac3fce6e161c3d
Peter N. Trinh <dayuse@gmail.com>
2012-09-11 17:48:55 -0300

    Add top/bottom margin to skyscraper

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div id="mm-sidebar">
 
 <!-- Project Wonderful Ad Box Code -->
-<div style="text-align:center;margin:0 auto;padding:3px 0;background-color:#504E64;"><div style="display:inline-block;" id="pw_adbox_65742_3_0"></div></div>
+<div style="text-align:center;margin:3px auto;padding:3px 0;background-color:#504E64;"><div style="display:inline-block;" id="pw_adbox_65742_3_0"></div></div>
 <!-- End Project Wonderful Ad Box Code -->
 
 	<?php dynamic_sidebar(); ?>
