21c197ae1633c8ff956f1c15e3162b48c6572dc4
Peter N. Trinh <dayuse@gmail.com>
2012-09-11 18:00:24 -0300

    Adjust skyscraper spacing, change colours

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div id="mm-sidebar">
 
 <!-- Project Wonderful Ad Box Code -->
-<div style="text-align:center;margin:10px auto;padding:6px;background-color:#504E64;"><div style="display:inline-block;margin:0 auto;" id="pw_adbox_65742_3_0"></div></div>
+<div style="text-align:center;margin:10px auto;"><div style="display:inline-block;border:1px solid #504E64;" id="pw_adbox_65742_3_0"></div></div>
 <!-- End Project Wonderful Ad Box Code -->
 
 	<?php dynamic_sidebar(); ?>
