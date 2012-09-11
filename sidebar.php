b9448f4ceb1abf5f5cecd0ea65b00e27b026b55b
Peter N. Trinh <dayuse@gmail.com>
2012-09-11 17:30:45 -0300

    Add Project Wonderful skyscraper space

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,12 @@
 <div id="mm-sidebar">
+
+<!-- Project Wonderful Ad Box Code -->
+<div id="pw_adbox_65742_3_0"></div>
+<script type="text/javascript"></script>
+<noscript><map name="admap65742" id="admap65742"><area href="http://www.projectwonderful.com/out_nojs.php?r=0&c=0&id=65742&type=3" shape="rect" coords="0,0,160,600" title="" alt="" target="_blank" /></map>
+<table cellpadding="0" cellspacing="0" style="width:160px;border-style:none;background-color:#ffffff;"><tr><td><img src="http://www.projectwonderful.com/nojs.php?id=65742&type=3" style="width:160px;height:600px;border-style:none;" usemap="#admap65742" alt="" /></td></tr><tr><td style="background-color:#ffffff;" colspan="1"><center><a style="font-size:10px;color:#0000ff;text-decoration:none;line-height:1.2;font-weight:bold;font-family:Tahoma, verdana,arial,helvetica,sans-serif;text-transform: none;letter-spacing:normal;text-shadow:none;white-space:normal;word-spacing:normal;" href="http://www.projectwonderful.com/advertisehere.php?id=65742&type=3" target="_blank">Ads by Project Wonderful!  Your ad here, right now: $0</a></center></td></tr></table>
+</noscript>
+<!-- End Project Wonderful Ad Box Code -->
+
 	<?php dynamic_sidebar(); ?>
 </div>
