b5e4c213392e6daddbfa62eb62e3a20d37ed71f9
Peter N. Trinh <dayuse@gmail.com>
2012-09-12 05:18:47 -0300

    Hide skyscraper ad

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,9 @@
 <div id="mm-sidebar">
 
 <!-- Project Wonderful Ad Box Code -->
+<!--
 <div style="text-align:center;margin:5px auto;"><div style="display:inline-block;" id="pw_adbox_65742_3_0"></div></div>
+-->
 <!-- End Project Wonderful Ad Box Code -->
 
 	<?php dynamic_sidebar(); ?>
